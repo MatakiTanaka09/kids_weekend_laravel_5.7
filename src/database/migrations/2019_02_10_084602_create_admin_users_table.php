099c91ad8bc084a2b559ee12b75fd240e933890f
Mataki Tanaka <mataki.tanaka.works@gmail.com>
2019-02-10 17:46:35 +0900

    Migrationファイル作成

---
+++ src/database/migrations/2019_02_10_084602_create_admin_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adminUsers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adminUsers');
+    }
+}
